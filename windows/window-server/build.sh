9aa67f734a76c46d495b08d18b9ce7cb266e7bd3
dongdpit <165973148+dongdpit@users.noreply.github.com>
2025-10-06 17:09:01 +0700

    Delete windows directory

--- windows/window-server/build.sh
+++
@@ -1,3 +0,0 @@
-packer build \
-  -var='os_iso_checksum=4f1457c4fe14ce48c9b2324924f33ca4f0470475e6da851b39ccbf98f44e7852' \
-  -var='os_iso_url=https://software-download.microsoft.com/download/sg/20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso' .
--- windows/window-server/data/autounattend.pkrtpl.hcl
+++
@@ -1,1 +0,0 @@
-https://github.com/vmware-samples/packer-examples-for-vsphere/blob/develop/builds/windows/server/2022/data/autounattend.pkrtpl.hcl
